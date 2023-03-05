d995c4b79f69ca90d24887a3e56f72e059c594b8
Gabriel Dantas <gabrieldantaslp@gmail.com>
2023-03-05 00:45:45 -0300

    Metodo create para chamado

--- back-end/src/main/java/com/gabriel/backend/resource/ChamadoResource.java
+++ back-end/src/main/java/com/gabriel/backend/resource/ChamadoResource.java
@@ -1,14 +1,20 @@
 package com.gabriel.backend.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gabriel.backend.domain.Chamado;
 import com.gabriel.backend.domain.dtos.ChamadoDTO;
@@ -32,6 +38,13 @@ public class ChamadoResource {
 	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
 		Chamado obj = chamadoService.findById(id);
 		return ResponseEntity.ok().body(new ChamadoDTO(obj));
+	}
+
+    @PostMapping
+	public ResponseEntity<ChamadoDTO> chamadoCreate(@Valid @RequestBody ChamadoDTO obj) {
+		Chamado newObj = chamadoService.chamadoCreate(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
     
 }
--- back-end/src/main/java/com/gabriel/backend/services/ChamadoService.java
+++ back-end/src/main/java/com/gabriel/backend/services/ChamadoService.java
@@ -1,12 +1,20 @@
 package com.gabriel.backend.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.backend.domain.Chamado;
+import com.gabriel.backend.domain.Cliente;
+import com.gabriel.backend.domain.Tecnico;
+import com.gabriel.backend.domain.dtos.ChamadoDTO;
+import com.gabriel.backend.domain.enums.Prioridade;
+import com.gabriel.backend.domain.enums.Status;
 import com.gabriel.backend.repositories.ChamadoRepository;
 import com.gabriel.backend.services.exceptions.ObjectnotFoundException;
 
@@ -16,6 +24,12 @@ public class ChamadoService {
     @Autowired
 	private ChamadoRepository chamadoRepository;
 
+    @Autowired
+	private TecnicoService tecnicoService;
+
+	@Autowired
+	private ClienteService clienteService;
+
     public List<Chamado> findAll() {
 		return chamadoRepository.findAll();
 	}
@@ -23,6 +37,32 @@ public class ChamadoService {
     public Chamado findById(Integer id) {
 		Optional<Chamado> obj = chamadoRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto de id: " + id + " não encontrado!"));
+	}
+
+    public Chamado chamadoCreate(@Valid ChamadoDTO obj) {
+        return chamadoRepository.save(newChamado(obj));
+    }
+
+    private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		
+		Chamado chamado = new Chamado();
+		if(obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		
+		if(obj.getStatus().equals(2)) {
+			chamado.setDataFechamento(LocalDate.now());
+		}
+		
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
 	}
     
 }
