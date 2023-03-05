4fd405f77a24b110dc5dffcb3c8e46b69fd8d582
Gabriel Dantas <gabrieldantaslp@gmail.com>
2023-03-05 12:01:03 -0300

    Importando modulo para trabalhar com formularios

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 // Modulo para requisições HTTP
 import { HttpClientModule } from '@angular/common/http';
 
+// Modulo para trabalhar com formulários
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -20,7 +23,10 @@ import { MaterialModule } from './shared/material/material.module';
     // Modulo do Angular Material
     MaterialModule,
     // Requisições http
-    HttpClientModule
+    HttpClientModule,
+    // Forms
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
