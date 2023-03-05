7ee42a0fe23fb50eebfad6192ca9f0f7ce49ea45
Gabriel Dantas <gabrieldantaslp@gmail.com>
2023-03-04 23:47:15 -0300

    Adicionando sobrecarga para o construtor com parametro DTO

--- back-end/src/main/java/com/gabriel/backend/domain/Tecnico.java
+++ back-end/src/main/java/com/gabriel/backend/domain/Tecnico.java
@@ -2,11 +2,13 @@ package com.gabriel.backend.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.gabriel.backend.domain.dtos.TecnicoDTO;
 import com.gabriel.backend.domain.enums.Perfil;
 
 import lombok.Getter;
@@ -31,6 +33,17 @@ public class Tecnico extends Pessoa {
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
+	}
+
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 }
